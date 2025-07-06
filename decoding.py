a54fccae76004a08e4de65064836941f48f74474
Agum <111370566+agumgmr@users.noreply.github.com>
2025-07-07 00:09:04 +0700

    Update decoding.py

--- decoding.py
+++ decoding.py
@@ -16,7 +16,7 @@ data, vertices_array, binary_qrcode = detector.detectAndDecode(image)
 # if there is a QR code
 # print the data
 if vertices_array is not None:
-  print("QRCode data:")
-  print(data)
+    print("QRCode data:")
+    print(data)
 else:
-  print("There was some error")
+    print("There was some error")
